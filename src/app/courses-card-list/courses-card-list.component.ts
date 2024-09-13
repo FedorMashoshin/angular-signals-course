2c9dfb1ffaae51f3c0621d8cd6c21647b41ef437
FedorMAshoshin <fmashoshin@gmail.com>
2024-09-12 23:01:50 -0700

    main page UI

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,7 +1,7 @@
-import {Component, inject, input, output} from '@angular/core';
-import {RouterLink} from "@angular/router";
-import {Course} from "../models/course.model";
-import {MatDialog} from "@angular/material/dialog";
+import { Component, inject, input, output } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { Course } from "../models/course.model";
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
   selector: 'courses-card-list',
@@ -13,5 +13,6 @@ import {MatDialog} from "@angular/material/dialog";
   styleUrl: './courses-card-list.component.scss'
 })
 export class CoursesCardListComponent {
+  courses = input<Course[]>();
 
 }
