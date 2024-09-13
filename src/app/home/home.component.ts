c380d2d011b748fcd4f87f6bb98dc3904d3dde3e
FedorMAshoshin <fmashoshin@gmail.com>
2024-09-12 22:47:37 -0700

    BE calls implementation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import { Component, computed, effect, inject, Injector, signal } from '@angular/core';
+import { CoursesService } from "../services/courses.service";
+import { Course, sortCoursesBySeqNo } from "../models/course.model";
+import { MatTab, MatTabGroup } from "@angular/material/tabs";
+import { CoursesCardListComponent } from "../courses-card-list/courses-card-list.component";
+import { MatDialog } from "@angular/material/dialog";
+import { MessagesService } from "../messages/messages.service";
+import { catchError, from, throwError } from "rxjs";
+import { toObservable, toSignal, outputToObservable, outputFromObservable } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'home',
@@ -20,5 +20,27 @@ import {toObservable, toSignal, outputToObservable, outputFromObservable} from "
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  #courses = signal<Course[]>([]);
+  beginnerCourses = computed(() => this.#courses().filter(course => course.category === 'BEGINNER'));
+  advancedCourses = computed(() => this.#courses().filter(course => course.category === 'ADVANCED'));
 
+  coursesService = inject(CoursesService);
+
+  constructor() {
+    effect(() => {
+      console.log('beginnerCourses', this.beginnerCourses());
+      console.log('advancedCourses', this.advancedCourses());
+    });
+
+    this.loadCourses();
+  }
+
+  async loadCourses() {
+    try {
+      const courses = await this.coursesService.loadAllCourses();
+      this.#courses.set(courses);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
